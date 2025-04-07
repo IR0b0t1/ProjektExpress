a119e1da77a2c46f4fb2f6bb7cccc8a69eb80da6
FL <flitewka@gmail.com>
2025-04-07 14:15:53 +0200

    Every day we move further away from God

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('express-handlebars');
 const app = express()
 const PORT = 3000;
 const path = require("path");
+const fs = require("fs")
 app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));
 app.set('view engine', 'hbs');
@@ -10,12 +11,64 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-const context = ""
+let context = ""
+const dirArr = []
+const fileArr = []
 
 // GET-y
-app.get("/", function (req, res) {
+app.get("/filemanager", function (req, res) {
+    fs.readdir(`${__dirname}\\upload`, (err, files) => {
+        if (err) throw err
+        console.log("lista", files);
+
+        files.forEach((file) => {
+            fs.lstat(`upload/${file}`, (err, stats) => {
+                if (err) throw err
+                console.log(file, stats.isDirectory());
+                if (stats.isDirectory() == true) {
+                    console.log('true')
+                    dirArr.push(file)
+                } else {
+                    console.log('false')
+                    fileArr.push(file)
+                }
+                console.log(`Dirs: ${dirArr}`)
+                console.log(`Files: ${fileArr}`)
+            })
+        })
+    })
     res.render('index.hbs', context);
 })
+
+app.post("/addFolder", function (req, res) {
+    console.log('addFolder')
+    console.log(req.body)
+    const filepath = path.join(__dirname, "upload", req.body.name)
+    console.log(filepath)
+    try {
+        fs.mkdirSync(filepath)
+    }
+    catch (err) {
+        console.log("error", err.message);
+    }
+    res.render('index.hbs', context)
+})
+
+app.post("/addFile", function (req, res) {
+    console.log('addFile')
+    console.log(req.body)
+    const filepath = path.join(__dirname, "upload", req.body.name)
+    console.log(filepath)
+    fs.writeFile(filepath, '', (err) => {
+        if (err) throw err
+        console.log("plik nadpisany");
+    })
+    res.render('index.hbs', context)
+})
+
+// app.get("*", function (req, res) {
+//     res.render('404.hbs', context);
+// })
 
 // Static
 app.use(express.static('static'))
--- static/js/script.js
+++ static/js/script.js
@@ -18,14 +18,21 @@ const addFolder = () => {
         folderForm.action = "/addFolder"
 
         const folderInput = document.createElement('input')
-        folderInput.placeholder = 'File name'
+        folderInput.placeholder = 'Folder name'
+        folderInput.required = 'true'
+        folderInput.name = 'name'
         folderForm.appendChild(folderInput)
 
         const folderButtonCreate = document.createElement('button')
+        folderButtonCreate.textContent = 'Create'
         folderForm.appendChild(folderButtonCreate)
 
         const folderButtonCancel = document.createElement('button')
         folderButtonCancel.textContent = 'Cancel'
+        folderButtonCancel.onclick = function () {
+            dialog.remove()
+        }
+
         folderForm.appendChild(folderButtonCancel)
 
         dialog.id = 'dialog'
@@ -40,6 +47,61 @@ const addFolder = () => {
         dialog.style.justifyContent = 'center'
         dialog.style.alignItems = 'center'
         dialog.appendChild(folderForm)
+        document.body.appendChild(dialog);
+    } else {
+        dialog.remove()
+    }
+}
+
+const addFile = () => {
+    let dialog = document.getElementById('dialog')
+    if (dialog == null) {
+        dialog = document.createElement("DIALOG");
+
+        const folderP = document.createElement("p");
+        folderP.style.background = 'rgb(222,222,234)'
+        folderP.style.color = 'darkblue'
+        folderP.style.width = '200px'
+        folderP.style.height = '50px'
+        folderP.style.display = 'flex'
+        folderP.style.justifyContent = 'center'
+        folderP.style.alignItems = 'center'
+        folderP.textContent = 'New file name:'
+
+        const fileForm = document.createElement('form')
+        fileForm.method = "POST"
+        fileForm.action = "/addFile"
+
+        const fileInput = document.createElement('input')
+        fileInput.placeholder = 'File name'
+        fileInput.required = 'true'
+        fileInput.name = 'name'
+        fileForm.appendChild(fileInput)
+
+        const folderButtonCreate = document.createElement('button')
+        folderButtonCreate.textContent = 'Create'
+        fileForm.appendChild(folderButtonCreate)
+
+        const folderButtonCancel = document.createElement('button')
+        folderButtonCancel.textContent = 'Cancel'
+        folderButtonCancel.onclick = function () {
+            dialog.remove()
+        }
+
+        fileForm.appendChild(folderButtonCancel)
+
+        dialog.id = 'dialog'
+        dialog.setAttribute('open', 'open');
+        dialog.appendChild(folderP);
+        dialog.style.zIndex = 100;
+        dialog.style.margin = 'auto'
+        dialog.style.width = '600px'
+        dialog.style.height = '400px'
+        dialog.style.display = 'flex'
+        dialog.style.flexDirection = 'column'
+        dialog.style.justifyContent = 'center'
+        dialog.style.alignItems = 'center'
+        dialog.appendChild(fileForm)
         document.body.appendChild(dialog);
     } else {
         dialog.remove()
